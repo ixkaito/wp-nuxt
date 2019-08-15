a28d479ca5f22c8a9f89dd2f62bd4423ecfa36b0
Kite <ixkaito@gmail.com>
2019-08-15 18:05:39 +0900

    Add featured media

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
   <div>
     <ul>
       <li v-for="post in posts">
+        <figure v-if="post._embedded['wp:featuredmedia']">
+          <img v-bind:src="post._embedded['wp:featuredmedia'][0].media_details.sizes.medium_large.source_url" alt="Post thumbnail">
+        </figure>
         <h2>{{ post.title.rendered }}</h2>
         <div v-html="post.excerpt.rendered"></div>
       </li>
@@ -12,7 +15,7 @@
 <script>
 export default {
   async asyncData({ $axios }) {
-    const { data } = await $axios.get('/posts')
+    const { data } = await $axios.get('/posts?_embed')
     return { posts: data }
   }
 }
