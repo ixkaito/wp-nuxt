06395e10b41be6d4d5c9c33ad48b696280b5c4c3
Kite <ixkaito@gmail.com>
2019-08-16 00:50:51 +0900

    Update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'WordPress + Nuxt.js',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'WordPress + Nuxt.js live demo for CaT vol.7' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
