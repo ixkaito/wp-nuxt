02f75bd78bc0fc1c51a2bf906cfbdaf015eb3087
Kite <ixkaito@gmail.com>
2019-08-15 17:28:43 +0900

    Add post list

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="post in posts">
+        <h2>{{ post.title.rendered }}</h2>
+        <div v-html="post.excerpt.rendered"></div>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('/posts')
+    return { posts: data }
+  }
+}
+</script>
