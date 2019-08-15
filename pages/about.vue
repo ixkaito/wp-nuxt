ee1604e08f13f6ec1dbd629ea229d390a74ecd6b
Kite <ixkaito@gmail.com>
2019-08-15 16:50:56 +0900

    Add about page and API

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>{{ page.title.rendered }}</h2>
+    <div v-html="page.content.rendered"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('/pages/2')
+    return { page: data }
+  }
+}
+</script>
