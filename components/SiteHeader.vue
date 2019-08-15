0998e67f9ce76641c58bf6dafcab72b4fe3a6261
Kite <ixkaito@gmail.com>
2019-08-15 14:48:11 +0900

    Add site header component and update layout

---
+++ components/SiteHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>WordPress + Nuxt.js</h1>
+    <nav>
+      <n-link to="/">Home</n-link>
+      <n-link to="/about/">About</n-link>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
